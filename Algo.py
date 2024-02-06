ba5d2c0eaffbf27c9bfbdd3f40a8f6eb27298c21
Soumeya-gh <soumeya.ghalem@le-campus-numerique.fr>
2024-02-06 16:48:40 +0100

    Alor Insert en cours

--- Algo.py
+++ Algo.py
@@ -1,7 +1,7 @@
 print('Algo de tri')
 
-#Variable
-def algoTri1 ():
+#Sorting Algorithm
+def algoSort ():
     i = 0
     tab_list = [8,7,6,5,4,3,2,1,0]
     for i in range (0,len(tab_list)):
@@ -11,8 +11,21 @@ def algoTri1 ():
                 tab_list[j], tab_list[i] = tab_list[i], tab_list[j]
 
     return tab_list
-print(algoTri1())
+print(algoSort())
 
+#Insert Algorithm
+
+def algoInsert ():
+    T = [9,8,7,6,5,4,3,2,1,0]
+    for i in range(1, len(9)):
+        moove = T[i]
+        j = i
+        while (j >=0 and T[j-1] >moove) :
+            T[j] = T[j-1]
+            j = j-1
+            T[j] = moove
+    return T
+print(algoInsert())
 
 
 
