c307985cdebf1a5d3d7bbe34810d7b09f9a1bf39
Soumeya-gh <soumeya.ghalem@le-campus-numerique.fr>
2024-02-12 10:19:28 +0100

    Fin Algo de Tri

--- Algo.py
+++ Algo.py
@@ -1,37 +1,56 @@
 print('Algo de tri')
 
-#Sorting Algorithm
-def algoSort ():
-    i = 0
-    tab_list = [8,7,6,5,4,3,2,1,0]
-    for i in range (0,len(tab_list)):
-        j= i
-        for j in range (0,len(tab_list)):
-            if (tab_list[j] >  tab_list [i]) :
-                tab_list[j], tab_list[i] = tab_list[i], tab_list[j]
 
-    return tab_list
-print(algoSort())
-
-#Insert Algorithm
-
-def algoInsert ():
-    T = [9,8,7,6,5,4,3,2,1,0]
-    for i in range(1, len(9)):
-        moove = T[i]
+# Insert Algorithm
+def insertSort(tab):
+    for i in range(0, len(tab)):
+        passage = tab[i]
         j = i
-        while (j >=0 and T[j-1] >moove) :
-            T[j] = T[j-1]
-            j = j-1
-            T[j] = moove
-    return T
-print(algoInsert())
+        while j > 0 and tab[j - 1] > passage:
+            tab[j] = tab[j - 1]
+            j = j - 1
+            tab[j] = passage
+    return tab
 
 
+# Ex
+tab = [78, 58, 41, 26, 15]
+print(insertSort(tab))
 
 
+# -----------------------------------------------#
+
+# Selection sort
+def algoSort(list):
+    i = 0
+    for i in range(0, len(list)):
+        j = i
+        for j in range(0, len(list)):
+            if list[j] > list[i]:
+                list[j], list[i] = list[i], list[j]
+
+    return list
 
 
+# Ex
+list = [5, 4, 3, 2, 1, 0]
+print(algoSort(list))
 
 
+# -----------------------------------------------#
 
+# Bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+# Ex
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Le résultat ", arr)
+
+# -----------------------------------------------#
